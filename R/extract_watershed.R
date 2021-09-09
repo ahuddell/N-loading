70f74ee8da80e31baf58b41f3412e28d2fc7d3ff
ahuddell <48715797+ahuddell@users.noreply.github.com>
2021-09-09 09:23:43 -0400

    adding basic maps

--- R/extract_watershed.R
+++ R/extract_watershed.R
@@ -3,10 +3,12 @@ library(tidyverse)
 library(sf)
 library(nhdplusTools)
 library(here)
+library(mapview)
+library(leaflet)
 
 #read data
 dat<-read_csv( file = here('data', 'ECHO_data_clean.csv'))
-
+write_rds(dat, paste0(here(), "/test.rds"))
 #remove NAs from lat/ long
 dat_noNA<- dat %>% drop_na(c('LONGITUDE_MEASURE','LATITUDE_MEASURE'))
 
@@ -16,5 +18,27 @@ dat_sf <- st_as_sf(dat_noNA, coords = c('LONGITUDE_MEASURE', 'LATITUDE_MEASURE')
 dat_sf
 
 
-#extract huc12 codes 
-nhdplusTools::get_huc12(AOI=dat_sf)
+#extract huc8 codes 
+HUC8<-nhdplusTools::get_huc12(AOI=dat_sf$geometry)
+
+#viewing data extents on map
+mapview(HUC8)
+mapview(dat_sf)
+
+
+## intersect polygons with points, keeping the information from both
+dat_join_sf = st_intersection(dat_sf, HUC8)
+
+
+#leaflet map
+leaflet() %>% 
+  #addTiles() %>%
+  addProviderTiles(providers$Esri.WorldTerrain) %>% 
+  addPolygons(data = HUC8$geometry) %>%
+  addCircleMarkers(data=dat_join_sf$geometry, col="red",
+                   fillOpacity = .2, radius = .5)
+
+## transform into a 'data.frame' by removing the geometry
+st_geometry(dat_join) = NULL
+head(dat_join)
+
