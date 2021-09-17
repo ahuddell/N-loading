c66ef3296765ad2a71aa8c4c834336fd66e81a3d
ahuddell <48715797+ahuddell@users.noreply.github.com>
2021-09-17 12:08:37 -0400

    basic functional choropleth map

--- R/extract_watershed.R
+++ R/extract_watershed.R
@@ -17,6 +17,11 @@ id<-c('01080101','01080102','01080103','01080104','01080105','01080106','0108010
 
 #NOTE: I removed HUC '01090005' because the AWS link is broken
 
+#creating directory to place the zipfiles
+#make this a temp folder later 
+#i.e. https://hydroecology.net/downloading-extracting-and-reading-files-in-r/
+dir.create(here("data", "dl files"))
+
 for (hucID in id){
   print(hucID)
   
@@ -106,29 +111,19 @@ huc8_02030201<-st_read(
   here('data','huc8 shapes',paste0('HUC8_','02030201'),'Shape'), 
   layer='WBDHU8')
 
+#join polygons
+huc8_combined <- dplyr::bind_rows(list(huc8_01080101,huc8_01080102,huc8_01080103,
+                                   huc8_01080104,huc8_01080105,huc8_01080106,
+                                   huc8_01080107,huc8_01080201,huc8_01080202,
+                                   huc8_01080203,huc8_01080204,huc8_01080205,
+                                   huc8_01080206,huc8_01080207,huc8_01100001,
+                                   huc8_01100002,huc8_01100003,huc8_01100004,
+                                   huc8_01100005,huc8_01100006,huc8_02030102,
+                                   huc8_02030201))
+mapview(huc8_combined) #need to add last missing watershed
+
 #now transforming all CRS
-huc8_01080101<-st_transform(huc8_01080101, crs=4326)
-huc8_01080102<-st_transform(huc8_01080102, crs=4326)
-huc8_01080103<-st_transform(huc8_01080103, crs=4326)
-huc8_01080104<-st_transform(huc8_01080104, crs=4326)
-huc8_01080105<-st_transform(huc8_01080105, crs=4326)
-huc8_01080106<-st_transform(huc8_01080106, crs=4326)
-huc8_01080107<-st_transform(huc8_01080107, crs=4326)
-huc8_01080201<-st_transform(huc8_01080201, crs=4326)
-huc8_01080202<-st_transform(huc8_01080202, crs=4326)
-huc8_01080203<-st_transform(huc8_01080203, crs=4326)
-huc8_01080204<-st_transform(huc8_01080204, crs=4326)
-huc8_01080205<-st_transform(huc8_01080205, crs=4326)
-huc8_01080206<-st_transform(huc8_01080206, crs=4326)
-huc8_01080207<-st_transform(huc8_01080207, crs=4326)
-huc8_01100001<-st_transform(huc8_01100001, crs=4326)
-huc8_01100002<-st_transform(huc8_01100002, crs=4326)
-huc8_01100003<-st_transform(huc8_01100003, crs=4326)
-huc8_01100004<-st_transform(huc8_01100004, crs=4326)
-huc8_01100005<-st_transform(huc8_01100005, crs=4326)
-huc8_01100006<-st_transform(huc8_01100006, crs=4326)
-huc8_02030102<-st_transform(huc8_02030102, crs=4326)
-huc8_02030201<-st_transform(huc8_02030201, crs=4326)
+huc8_combined<-st_transform(huc8_combined, crs=4326)
 
 
 # read in our data convert to sf object -----------------------------------
@@ -140,11 +135,13 @@ dat<-read_csv( file = here('data', 'ECHO_data_clean.csv'))
 dat_noNA<- dat %>% drop_na(c('LONGITUDE_MEASURE','LATITUDE_MEASURE'))
 
 dat_summary<- dat %>%
-  group_by(key,)
+  group_by(huc8,date) %>%
+  summarise(sum(kg_N_TN_per_month,na.rm=T))
 
 #convert to sf obj
 dat_sf <- st_as_sf(dat_noNA, coords = c('LONGITUDE_MEASURE', 'LATITUDE_MEASURE'), 
                    crs = 4326)
+
 dat_sf
 
 
@@ -152,57 +149,79 @@ dat_sf
 
 #viewing data extents on map
 mapview(dat_sf)
+# 
+# ## intersect polygons with points, keeping the information from both
+# dat_join_sf = st_intersection(dat_sf, HUC8)
+
+#calculating monthly TN totals by watershed
+dat_summary<- dat %>%
+  group_by(huc8,date) %>%
+  summarise(month_total_huc=sum(kg_N_TN_per_month,na.rm=T))
+
+#join huc spatial data to monthly totals
+N_load_huc_join<-left_join(dat_summary_test,huc8_combined)
+N_load_huc_join<-st_as_sf(N_load_huc_join)
+
+#subset of one month for mapping
+one_month_test<-filter(N_load_huc_join, date==max(date,na.rm = T))
 
 
+#color palette
+#bins <- unname(quantile(one_month_test$month_total_huc, c(.2,.6,.8,1)))
+bins <- c(0,2,3,6,7000,8000)
+pal <- colorBin("RdYlBu", domain = one_month_test$month_total_huc, bins = bins)
+#in future make this colorQuantiles with more categories
 
-## intersect polygons with points, keeping the information from both
-dat_join_sf = st_intersection(dat_sf, HUC8)
-
-# Create a color palette for the map:
-get_viridis_colors <- function(no_colors){
-  appsilon_col2Hex(viridis::viridis_pal(option = 'C')(no_colors))
-}
-
-#names of polygons to plot
-to_plot<-paste0('huc8_', c(id))
+labels <- sprintf(
+  "<strong>%s</strong><br/>%g kg total N / month<sup>-1</sup>",
+  one_month_test$name, one_month_test$month_total_huc
+) %>% lapply(htmltools::HTML)
 
 #leaflet map
-leaflet() %>% 
-  #addTiles() %>%
-  addProviderTiles(providers$OpenStreetMap) %>% 
-  addPolygons(data = huc8_01080101$geometry) %>%
-  addPolygons(data = huc8_01080102$geometry) %>%
-  addPolygons(data = huc8_01080103$geometry) %>%
-  addPolygons(data = huc8_01080104$geometry) %>%
-  addPolygons(data = huc8_01080105$geometry) %>%
-  addPolygons(data = huc8_01080106$geometry) %>%
-  addPolygons(data = huc8_01080107$geometry) %>%
-  addPolygons(data = huc8_01080201$geometry) %>%
-  addPolygons(data = huc8_01080202$geometry) %>%
-  addPolygons(data = huc8_01080203$geometry) %>%
-  addPolygons(data = huc8_01080204$geometry) %>%
-  addPolygons(data = huc8_01080205$geometry) %>%
-  addPolygons(data = huc8_01080206$geometry) %>%
-  addPolygons(data = huc8_01080207$geometry) %>%
-  addPolygons(data = huc8_01100001$geometry) %>%
-  addPolygons(data = huc8_01100002$geometry) %>%
-  addPolygons(data = huc8_01100003$geometry) %>%
-  addPolygons(data = huc8_01100004$geometry) %>%
-  addPolygons(data = huc8_01100005$geometry) %>%
-  addPolygons(data = huc8_01100006$geometry) %>%
-  addPolygons(data = huc8_02030102$geometry) %>%
-  addPolygons(data = huc8_02030201$geometry) %>%
-  addCircleMarkers(data=dat_sf$geometry, col='red',
-                   fillOpacity = .2, radius = .5) %>%
-  setView( lat=43, lng=-73.2 , zoom=7) 
+m <- leaflet(one_month_test) %>%
+  addProviderTiles(providers$OpenStreetMap) %>%
+  addPolygons()   %>%
+  setView(lat = 43,
+          lng = -73.2 ,
+          zoom = 6.5) %>%
+  addPolygons(
+    fillColor = ~ pal(one_month_test$month_total_huc),
+    weight = 2,
+    color = 'black',
+    opacity = 1,
+    fillOpacity = 0.7,
+    label = labels,
+    labelOptions = labelOptions(
+      style = list("font-weight" = "normal", padding = "3px 8px"),
+      textsize = "15px",
+      direction = "auto")
+  ) %>%
+  addCircleMarkers(
+    data = dat_sf$geometry,
+    col = 'darkgrey',
+    fillOpacity = .8,
+    radius = .5
+  ) %>% addLegend(pal = pal, values = ~density, opacity = 0.7, title = NULL,
+                  position = "bottomright")
+m
 
-#
+mapshot(map_with_points, file='huc8_observation_points.jpeg')
 
 
-addPolygons( fillColor = ~mypalette(kg_N_TN_per_month), stroke=FALSE )%>%
-  addLegend(position = 'bottomright',
-            colors = get_viridis_colors(2),
-            labels = types)
+
+# #names of polygons to plot
+# to_plot<-paste0('huc8_', c(id))
+# 
+# # Create a color palette for the map:
+# get_viridis_colors <- function(no_colors){
+#   appsilon_col2Hex(viridis::viridis_pal(option = 'C')(no_colors))
+# }
+# 
+# addPolygons( fillColor = ~mypalette(kg_N_TN_per_month), stroke=FALSE )%>%
+#   addLegend(position = 'bottomright',
+#             colors = get_viridis_colors(2),
+#             labels = types)
+
 
 
 
