47e19dd03908c7f2b033a767e5efaac2ab2cf063
ahuddell <amh2284@columbia.edu>
2021-10-01 12:05:28 -0400

    took LIS title off for draft app

--- R/app.R
+++ R/app.R
@@ -1,117 +1,207 @@
 
+# load libraries ----------------------------------------------------------
 library(shinydashboard)
 library(shinydashboardPlus)
+library(shinythemes)
 library(shiny)
 library(tidyverse)
 library(RColorBrewer)
 library(bslib)
 # library(highcharter)
-#library(shinyjs)
+# library(shinyjs)
 # library(tmap)
 # library(tmaptools)
 library(leaflet)
 library(here)
 library(lubridate)
 library(sf)
-# library(mapdeck)
+library(viridis)
 
-# data and headers
-title <- tags$a(
-  href = "https://www.longislandsoundstudy.net",
-  icon("fish"), "Long Island Sound Study"
-)
-
+# read data, objects, and functions ---------------------------------------
 # load in spatial dataframe with summarized monthly N loads by HUC8 watershed
-dat <- st_read(here(
+dat_sf <- st_read(here(
   "data", "huc_8_dat_join",
   "N_load_huc_join.shp"
 ))
 
-# using smaller subset of data for building app
-# dat <-
-#   filter(
-#     dat,
-#     dat$date > "2005-01-01",
-#     dat$date < "2008-01-01",
-#   )
+# same data as data frame object for plots
+dat <- as.data.frame(dat_sf)
 
-st_crs(dat) # WGS 84 is coordinate reference system
-
-labels <- sprintf(
-  "<strong>%s</strong><br/>%g kg total N / month<sup>-1</sup>",
-  dat$name, dat$kgN_huc8
-) %>% lapply(htmltools::HTML)
-
-
-# bins <- unname(quantile(one_month_test$kgN_huc8, c(.2,.6,.8,1)))
+# color palette for choropleth map
 bins <- c(0, 2, 3, 6, 7000, 8000)
 pal <- colorBin("Blues", bins = bins)
 # in future make this colorQuantiles with more categories
 
 
-ui <- fluidPage(
-  theme=bs_theme(version=4,bootswatch = "lumen"),
-  # tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+# # labels for map
+# labels <- sprintf(
+#   "<strong>%s</strong><br/>%g kg total N / month<sup>-1</sup>",
+#   dat_sf$name, dat_sf$kgN_huc8
+# ) %>% lapply(htmltools::HTML)
 
+centroids <- dat_sf%>%
+  group_by(name) %>%
+  slice(1) %>%
+  st_centroid()
+
+# ui ----------------------------------------------------------------------
+ui <- function(request) {
+  navbarPage(
+    title = tags$a(
+     # href = "https://www.longislandsoundstudy.net",
+      icon("fish", lib = "font-awesome"),
+     # "Long Island Sound Study"
+     "Draft app"
+    ),
+    theme = shinytheme("lumen"),
+    tabPanel(
+      "Point Source N loading",
+      fluidRow(
+        height = 4,
+        tags$style(type = "text/css", ".selectize-input{ z-index: 999; }"),
+        h3("N loading"),
+        p(
+          "This is a data visualization tool summarizing monthly point source N loading in the Long Island Sound, and summer month hypoxia area and volume. Results from each analysis are in separate tabs"
+        ),
+        column(
+          width = 6,
+          sliderInput(
+            "month",
+            "Month",
+            min = min(dat$date),
+            max = max(dat$date),
+            value = median(dat$date),
+            timeFormat = "%b %Y",
+            step = 1
+          )
+        ),
+        column(
+          width = 6,
+          selectizeInput(
+            "watershed_name",
+            "Watershed",
+            choices = dat$name,
+            multiple = T,
+            selected = "Housatonic"
+          )
+        )
+      ),
+      fluidRow(
+        splitLayout(
+          cellWidths = c("50%", "50%"),
+          leafletOutput("map"),
+          plotOutput("plot1")
+        )
+      ),
+    )
   )
-  
+}
 
 
+
+# server ------------------------------------------------------------------
+
+
+# Server
 server <- function(input, output, session) {
 
+  ### Tab 1
   # Reactive expression for the data subsetted to what the user selected
-  filteredData <- reactive({
-    filter(dat, date == input$month)
-  })
-  
-  filteredLabels <- reactive({
-      sprintf(
-          "<strong>%s</strong><br/>%g kg total N / month<sup>-1</sup>",
-          dat$name, dat$kgN_huc8
-      ) %>% lapply(htmltools::HTML)
+  filtered_month <- reactive({
+    filter(dat_sf, date == input$month)
   })
 
+  # filteredLabels <- reactive({
+  #   filter(
+  #     sprintf(
+  #       "<strong>%s</strong><br/>%g kg total N / month<sup>-1</sup>",
+  #       dat$name,
+  #       dat$kgN_huc8
+  #     ) %>% lapply(htmltools::HTML)
+  #   )
+  # })
+  #
+  # render base map
   output$map <- renderLeaflet({
-    # Use leaflet() here, and only include aspects of the map that
-    # won't need to change dynamically (at least, not unless the
-    # entire map is being torn down and recreated).
-    leaflet(dat) %>%
-      addProviderTiles(providers$OpenStreetMap) %>%
+    leaflet(dat_sf) %>%
+      addProviderTiles(providers$Esri) %>%
       addLegend(
         pal = pal,
-        values = ~density, opacity = 0.7, title = NULL,
-        position = "bottomright"
-      ) %>%
-      fitBounds(
-        ~ unname(st_bbox(dat)[1]), ~ unname(st_bbox(dat)[2]),
-        ~ unname(st_bbox(dat)[3]), ~ unname(st_bbox(dat)[4])
+        values = ~ density,
+        opacity = 0.7,
+        title = NULL,
+        position = "bottomright") %>%
+      fitBounds(~ unname(st_bbox(dat_sf)[1]),
+                ~ unname(st_bbox(dat_sf)[2]),
+                ~ unname(st_bbox(dat_sf)[3]),
+                ~ unname(st_bbox(dat_sf)[4])) %>%
+
+      addLabelOnlyMarkers(
+        data = centroids,
+        lng = ~ unlist(map(centroids$geometry, 1)),
+        lat = ~ unlist(map(centroids$geometry, 2)),
+        label = ~ (name),
+        labelOptions = labelOptions(textsize = 6,
+          noHide = T,
+          direction = 'center',
+          textOnly = TRUE,
+          
+        )
       )
+
   })
-
-
-
-
-  # Incremental changes to the map (in this case, replacing the
-  # circles when a new color is chosen) should be performed in
-  # an observer. Each independent set of things that can change
-  # should be managed in its own observer.
+  
+  # add reactive choropleth layer
   observe({
-    leafletProxy("map", data = filteredData()) %>%
+    leafletProxy("map", data = filtered_month()) %>%
       clearShapes() %>%
       addPolygons(
-        fillColor = ~pal(kgN_huc8),
+        fillColor = ~ pal(kgN_huc8),
         weight = 2,
         color = "black",
         opacity = 1,
-        fillOpacity = 0.7,
-        # label = filteredLabels(),
-        # labelOptions = labelOptions(
-        #     style = list("font-weight" = "normal", padding = "3px 8px"),
-        #     textsize = "15px",
-        #     direction = "auto"
-        # )
-      )
+        fillOpacity = 0.7)
+        
+      #   labelOptions = labelOptions(
+      #       style = list("font-weight" = "normal", padding = "3px 8px"),
+      #       textsize = "15px",
+      #       direction = "auto"
+      #   )
+      #   # popup = ~paste0("Site No: ", SiteID)
+      # )
   })
+
+  
+
+  # Reactive expression for the data subsetted to what the user selected
+  filtered_watershed_name <- reactive({
+    dat %>%
+      filter(name == input$watershed_name) %>%
+      droplevels()
+  })
+
+
+  # render plot
+  output$plot1 <- renderPlot(
+    ggplot(
+      data = filtered_watershed_name(),
+      aes(x = date, y = kgN_huc8, col = name,
+          fill=name)) +
+      ylab(expression(paste(
+        'Monthly N load (kg N ',ha^-1,yr^-1,')')))+
+      xlab('Date')+
+      geom_point(alpha = 0.7) +
+      geom_smooth(method = "loess", se = T) +
+      theme_minimal() +
+      theme(text = element_text(size=18),
+            legend.position = "bottom")+
+      scale_color_viridis(name="Watershed", discrete=TRUE) +
+      scale_fill_viridis(name="Watershed", discrete=TRUE)
+  )
 }
+
+
+# run app -----------------------------------------------------------------
+
 
 shinyApp(ui, server)
