a53355fee68bb0504585e20e71ba403f76845456
ahuddell <48715797+ahuddell@users.noreply.github.com>
2021-10-15 12:47:01 -0400

    download recent data from ECHO API in two stages

--- R/echor_downloads.R
+++ R/echor_downloads.R
@@ -4,115 +4,104 @@ library(echor)
 library(tidyverse)
 library(here)
 
-requestURL <- function(query) {
-  
-  urlBuildList <- structure(list(scheme = "https",
-                                 hostname = "echodata.epa.gov",
-                                 port = NULL, 
-                                 path = "echo/cwa_rest_services.get_download", 
-                                 query = query), 
-                            class = "url")
-  return(httr::build_url(urlBuildList))
-}
-
-getDownload <- function(service, qid, qcolumns, col_types = NULL) {
-  ## build the request URL statement
-  if (service == "sdw") {
-    path <- "echo/sdw_rest_services.get_download"
-  } else if (service == "cwa") {
-    path <- "echo/cwa_rest_services.get_download"
-  } else if (service == "caa") {
-    path <- "echo/air_rest_services.get_download"
-  } else {
-    stop("internal error in getDownload, incorrect service argument supplied")
-  }
-  qid <- paste0("qid=", qid)
-  query <- paste(qid, qcolumns, sep = "&")
-  getURL <- requestURL(path = path, query = query)
-  
-  ## Make the request
-  request <- httr::RETRY("GET", getURL)
-  
-  ## Check for valid response for serve, else returns error
-  resp_check(request)
-  
-  info <- httr::content(request, as = "raw")
-  
-  info <- readr::read_csv(info, col_names = TRUE,
-                          col_types = col_types,
-                          na = " ",
-                          locale = readr::locale(date_format = "%m/%d/%Y"))
-  
-  return(info)
-}
-
-getDownload(service="cwa", qid=df$id, qcolumns=c(1:11,14,23,24,25,26,30,36,58,60,63,64,65,67,86,206))
-
-#this was my old query that worked
-df <- tibble::tibble("id" = c('CT0000086','CT0000434',
-                              'CT0000582','CT0000744')
-)
-
-df <- downloadDMRs(df, id)
-df<-df$dmr
-
-echoGetEffluent(p_id = 'tx0119407', parameter_code = '50050')
-#df<-as_tibble(df)
-# df<-unnest(df)
-
-
-library(httr)
-r <- GET("http://httpbin.org/get")
-
+#read in list of permits to request
 ids<-read_csv(here('data','data_request_LIS_AH.csv'))
 
-ids_1_10<-ids[30,]
-df <- downloadDMRs(df=ids_1_10, iColumn=id, verbose=TRUE)
-df<-df$dmr
 
-library(purrr)    
-out_lst <- map(df$url, pfun)
-names(out_lst) <- df$ID 
-pfun <- possibly(f1, otherwise = NA)
-f1 <- function(urllink) {
-  openxlsx::read.xlsx(urllink)
-}
-
-f2 <-  function(urllink) {
-  
-  tryCatch(openxlsx::read.xlsx(urllink), 
-           error = function(e) message("error occured"))
-}
-out_lst2 <- lapply(df$url, f2)
-
-#df<-as_tibble(df)
-# df<-unnest(df)
+ids_1_1000<-ids[1:500,]
+df1 <- downloadDMRs(ids_1_1000, NPDES_ID, verbose=TRUE)
+df1 <- df1 %>%
+  tidyr::unnest(dmr)
 
 
-test<-df[[1]]
-str(test)
+ids_501_1000<-ids[501:1000,]
+df2 <- downloadDMRs(ids_501_1000, NPDES_ID, verbose=TRUE)
+df2 <- df2 %>%
+  tidyr::unnest(dmr)
+
+ids_1001_1500<-ids[1001:1500,]
+df3 <- downloadDMRs(ids_1001_1500, NPDES_ID, verbose=TRUE)
+df3 <- df3 %>%
+  tidyr::unnest(dmr)
+
+
+ids_1501_1851<-ids[1501:1851,]
+df4 <- downloadDMRs(ids_1501_1851, NPDES_ID, verbose=TRUE)
+df4 <- df4 %>%
+  tidyr::unnest(dmr) 
+
+df4 <- df4 %>%  select(-"{")
+
+dat1<-rbind(df1,df2) 
+dat2<-rbind(df3,df4) 
+
+dat1<-dat1%>% 
+  filter(perm_feature_type_code=="EXO") %>%
+  select(npdes_id,
+         date=monitoring_period_end_date,
+         outfall=perm_feature_nmbr,
+         parameter_code,	
+         parameter_desc,	
+         monitoring_location_code,
+         monitoring_location_desc,
+         standard_unit_desc,
+         statistical_base_code,	
+         statistical_base_short_desc,	
+         statistical_base_type_code,	
+         statistical_base_type_desc,
+         dmr_value_standard_units,
+         dmr_value_qualifier_code)
+#write_csv(dat1,'ECHOR_DL_1.csv')
+
+dat2<-dat2%>% 
+  filter(perm_feature_type_code=="EXO") %>%
+  select(npdes_id,
+         date=monitoring_period_end_date,
+         outfall=perm_feature_nmbr,
+         parameter_code,	
+         parameter_desc,	
+         monitoring_location_code,
+         monitoring_location_desc,
+         standard_unit_desc,
+         statistical_base_code,	
+         statistical_base_short_desc,	
+         statistical_base_type_code,	
+         statistical_base_type_desc,
+         dmr_value_standard_units,
+         dmr_value_qualifier_code)
+write_csv(dat2,'ECHOR_DL_2.csv')          
+
+#test<-echoGetEffluent(p_id='NY0201235')
+
+names(dat)
+min(dat$monitoring_period_end_date)
+max(dat$monitoring_period_end_date)
+
+echoWaterGetFacilityInfo(p_huc='01080101') #function that returns huc8 watershed codes: p_huc
+
+
 
 #link to data dictionary https://echo.epa.gov/help/reports/effluent-charts-help
 
-levels(as.factor(test$npdes_id)) #want this
-levels(as.factor(test$perm_feature_id)) #outfall IDs
-levels(as.factor(test$perm_feature_nmbr)) #this is outfall number
-levels(as.factor(test$perm_feature_type_code))
-levels(as.factor(test$perm_feature_type_desc))
-levels(as.factor(test$parameter_code))
-levels(as.factor(test$parameter_desc))
-levels(as.factor(test$monitoring_location_code)) #we want to filter this to level 0 only, effluent gross
-levels(as.factor(test$monitoring_location_desc))
-levels(as.factor(test$standard_unit_desc)) #these are the standardized units
-levels(as.factor(test$statistical_base_short_desc)) #this is the stat
-levels(as.factor(test$dmr_event_id)) # The unique ID identifying the DMR Event. A DMR Event is a DMR submission with a DMR period end date and DMR due date
-levels(as.factor(test$monitoring_period_end_date)) #this is the month
-levels(as.factor(test$dmr_form_value_id)) #not sure if we need this 
-levels(as.factor(test$dmr_value_id)) #not sure if we need this
-levels(as.factor(test$dmr_value_nmbr)) #this might be the data?
-levels(as.factor(test$dmr_unit_desc)) #here are units
-levels(as.factor(test$dmr_value_standard_units)) #this might be data?
-levels(as.factor(test$dmr_value_qualifier_code)) #this shows <>? qualifier codes
+levels(as.factor(dat$npdes_id)) #want this
+levels(as.factor(dat$perm_feature_id)) #outfall IDs
+levels(as.factor(dat$perm_feature_nmbr)) #this is outfall number
+levels(as.factor(dat$perm_feature_type_code)) #we should filter to "EXO"
+levels(as.factor(dat$perm_feature_type_desc))
+levels(as.factor(dat$parameter_code))
+levels(as.factor(dat$parameter_desc))
+levels(as.factor(dat$monitoring_location_code)) #we want to filter this to level 0 only, effluent gross
+levels(as.factor(dat$monitoring_location_desc))
+levels(as.factor(dat$standard_unit_desc)) #these are the standardized units
+levels(as.factor(dat$statistical_base_short_desc)) #this is the stat
+levels(as.factor(dat$dmr_event_id)) # The unique ID identifying the DMR Event. A DMR Event is a DMR submission with a DMR period end date and DMR due date
+levels(as.factor(dat$monitoring_period_end_date)) #this is the month
+levels(as.factor(dat$dmr_form_value_id)) #not sure if we need this 
+levels(as.factor(dat$dmr_value_id)) #not sure if we need this
+levels(as.factor(dat$dmr_value_nmbr)) #this might be the data?
+levels(as.factor(dat$dmr_unit_desc)) #here are units
+levels(as.factor(dat$dmr_value_standard_units)) #this might be data?
+levels(as.factor(dat$dmr_value_qualifier_code)) #this shows <>? qualifier codes
 
 
 
@@ -165,6 +154,5 @@ ggplot(levels, aes(stat,n))+
   geom_col() +
   ylab("count of observations")+
   theme(axis.text.x=element_text(angle = 30, hjust=1))
-
 
 
