2d989e340ad57381f510074550326f5d1a7afec0
ahuddell <48715797+ahuddell@users.noreply.github.com>
2021-10-18 10:08:34 -0400

    added functional but extremely slow tab 2 with CTDEEP pH and DO graph

--- R/app.R
+++ R/app.R
@@ -16,37 +16,47 @@ library(here)
 library(lubridate)
 library(sf)
 library(viridis)
+library(stringr)
 
 # read data, objects, and functions ---------------------------------------
 # load in spatial dataframe with summarized monthly N loads by HUC8 watershed
 dat_sf <- st_read(here(
-  "data", "huc_8_dat_join",
-  "N_load_huc_join.shp"
+  'data', 'huc_8_dat_join',
+  'N_load_huc_join.shp'
 ))
 dat_sf$year<-year(dat_sf$date) #add a year column
 
 # same data as data frame object for plots
-dat <- read_csv(here("data", "ECHO_data_clean_all.csv"))
+dat <- read_csv(here('data', 'ECHO_data_clean_all.csv'))
 
 dat_WQ1<- read_csv(here(
-  "data", "DEEP_WQ_thru_2015.csv"
+  'data', 'DEEP_WQ_thru_2015.csv'
 ))
 dat_WQ2<- read_csv(here(
-  "data", "DEEP_WQ_2015_present.csv"
+  'data', 'DEEP_WQ_2015_present.csv'
 ))
 
 dat_WQ<-rbind(dat_WQ1,dat_WQ2)
+rm(dat_WQ1)
+rm(dat_WQ2)
+#reducing size of dat_WQ for speed
+dat_WQ<-dat_WQ %>%select(pH, oxygen_concentration_in_sea_water,End_Date) 
+dat_WQ$oxygen_concentration_in_sea_water<-as.numeric(dat_WQ$oxygen_concentration_in_sea_water,na.rm=T)
+
+dat_WQ$End_Date<-as.character(map(strsplit(dat_WQ$End_Date, split = "T"), 1))
+dat_WQ$End_Date<-ymd(dat_WQ$End_Date)
+
+dat_WQ<-dat_WQ %>% filter(End_Date > '2021-06-01')
 
 # color palette for choropleth map
 bins <- c(0,25000,50000,100000,200000,300000,550500)
-pal <- colorBin("Blues",bin=bins, na.color = "808080",
-)
+pal <- colorBin('Blues',bin=bins, na.color = '808080')
 # in future make this colorQuantiles with more categories
 
 
 # # labels for map
 # labels <- sprintf(
-#   "<strong>%s</strong><br/>%g kg total N / month<sup>-1</sup>",
+#   '<strong>%s</strong><br/>%g kg total N / month<sup>-1</sup>',
 #   dat_sf$name, dat_sf$kgN_huc8
 # ) %>% lapply(htmltools::HTML)
 
@@ -59,95 +69,81 @@ centroids <- dat_sf%>%
 ui <- function(request) {
   navbarPage(
     title = tags$a(
-     # href = "https://www.longislandsoundstudy.net",
-      icon("fish", lib = "font-awesome"),
-     # "Long Island Sound Study"
-     "Draft app"
+     # href = 'https://www.longislandsoundstudy.net',
+      icon('fish', lib = 'font-awesome'),
+     # 'Long Island Sound Study'
+     'Draft app'
     ),
-    theme = shinytheme("lumen"),
+    theme = shinytheme('lumen'),
     tabPanel(
-      "Point Source N loading",
+      'Point Source N loading',
       fluidRow(
         height = 4,
-        tags$style(type = "text/css", ".selectize-input{ z-index: 999; }"),
-        h3("N loading"),
-        p(
-          "This is a data visualization tool summarizing monthly point source N loading in the Long Island Sound, and summer month hypoxia area and volume. Results from each analysis are in separate tabs"
+        tags$style(type = 'text/css', '.selectize-input{ z-index: 999; }'),
+        h3('N loading'),
+        p('This is a data visualization tool summarizing monthly point source N loading in the Long Island Sound, and summer month hypoxia area and volume. Results from each analysis are in separate tabs'
         ),
         column(
           width = 6,
           sliderInput(
-            inputId = "year",
-            label="Year",
+            inputId = 'year',
+            label='Year',
             min = min(dat_sf$year),
             max = max(dat_sf$year),
             value = min(dat_sf$year),
-           timeFormat = "%Y",
-           sep = "",
-          step = 1
-          )
+           timeFormat = '%Y',
+           sep = '',
+          step = 1)
         ),
-        column(
-          width = 6,
+        column(width = 6,
           selectizeInput(
-            "watershed_name",
-            "Watershed",
-            choices = dat$name,
+            inputId ='watershed_name',
+            label = 'Watershed',
+            choices = dat_sf$name,
             multiple = T,
-            selected = "Housatonic"
-          )
-        )
-      ),
-      fluidRow(
-        splitLayout(
-          cellWidths = c("50%", "50%"),
-          leafletOutput("map"),
-          plotOutput("plot1")
-        )
-      ),
-    ),
+            selected = 'Housatonic'))
+        ),
+      fluidRow(splitLayout(
+          cellWidths = c('50%', '50%'),
+          leafletOutput('map'),
+          plotOutput('plot1')))),
   
 #Tab 2
   tabPanel(
-    "pH and DO",
+    'pH and DO',
     fluidRow(
       height = 4,
-      tags$style(type = "text/css", ".selectize-input{ z-index: 999; }"),
-      h3("N loading"),
-      p(
-        "This is a data visualization tool summarizing CT DEEP "
-      ),
-      column(
-        width = 6,
-        selectInput(
-          inputId = "WQ_param",
-          label="Water Quality parameter",
-          choices =c('pH'="pH",
-          'Dissolved Oxygen mg/L'="corrected_oxygen"),
-          selected="corrected_oxygen"
-        )
+      tags$style(type = 'text/css', '.selectize-input{ z-index: 999; }'),
+      h3('N loading'),
+      p('This is a data visualization tool summarizing CT DEEP '
       ),
       column(
         width = 6,
         selectizeInput(
-          "watershed_name",
-          "Watershed",
-          choices = dat$name,
+          inputId = 'WQ_param',
+          label='Water Quality parameter',
+          choices =c('pH',
+          'Dissolved Oxygen mg/L'),
+          selected='pH')
+      ),
+      column(
+        width = 6,
+        selectizeInput(
+          inputId ='watershed_name',
+          label='Watershed',
+          choices = dat_sf$name,
           multiple = T,
-          selected = "Housatonic"
+          selected = 'Housatonic'
         )
       )
     ),
     fluidRow(
       splitLayout(
-        cellWidths = c("50%", "50%"),
-        plotOutput("plot2"),
-        plotOutput("plot3")
-      )
-    ),
+        cellWidths = c('50%', '50%'),
+        plotOutput('plot2'),
+        plotOutput('plot3')))
   )
-  )
-}
+)}
 
 
 
@@ -165,7 +161,7 @@ server <- function(input, output, session) {
   # filteredLabels <- reactive({
   #   filter(
   #     sprintf(
-  #       "<strong>%s</strong><br/>%g kg total N / month<sup>-1</sup>",
+  #       '<strong>%s</strong><br/>%g kg total N / month<sup>-1</sup>',
   #       dat$name,
   #       dat$kgN_huc8
   #     ) %>% lapply(htmltools::HTML)
@@ -181,7 +177,7 @@ server <- function(input, output, session) {
         values = ~ density,
         opacity = 0.7,
         title = NULL,
-        position = "bottomright") %>%
+        position = 'bottomright') %>%
       fitBounds(~ unname(st_bbox(dat_sf)[1]),
                 ~ unname(st_bbox(dat_sf)[2]),
                 ~ unname(st_bbox(dat_sf)[3]),
@@ -195,21 +191,18 @@ server <- function(input, output, session) {
         labelOptions = labelOptions(textsize = 6,
           noHide = T,
           direction = 'center',
-          textOnly = TRUE,
-
-        )
+          textOnly = TRUE)
       )
-
   })
 
   # add reactive choropleth layer
   observe({
-    leafletProxy("map", data = filtered_year()) %>%
+    leafletProxy('map', data = filtered_year()) %>%
       #clearShapes() %>%
       addPolygons(
         fillColor = ~ pal(kgN_huc8),
         weight = 2,
-        color = "black",
+        color = 'black',
         opacity = 1,
         fillOpacity = 0.7)
   })
@@ -217,31 +210,74 @@ server <- function(input, output, session) {
 
   # Reactive expression for the data subsetted to what the user selected
   filtered_watershed_name <- reactive({
-    dat %>%
-      filter(name == input$watershed_name) %>%
-      droplevels()
+    dat_sf %>%
+      filter(name == input$watershed_name)
   })
 
 
   # render plot1
-  output$plot1 <- renderPlot(
+  output$plot1 <- renderPlot({
     ggplot(
       data = filtered_watershed_name(),
-      aes(x = date, y = kg_N_TN_per_month, col = name,
+      aes(x = date, y = kgN_huc8, col = name,
           fill=name)) +
       ylab(expression(paste(
         'Monthly N load (kg N ',ha^-1,yr^-1,')')))+
       xlab('Date')+
       geom_point(alpha = 0.7) +
-      geom_smooth(method = "loess", se = T) +
+      geom_smooth(method = 'loess', se = T) +
       theme_minimal() +
       theme(text = element_text(size=18),
-            legend.position = "bottom")+
-      scale_color_viridis(name="Watershed", discrete=TRUE) +
-      scale_fill_viridis(name="Watershed", discrete=TRUE)
-  )
+            legend.position = 'bottom')+
+      scale_color_viridis(name='Watershed', discrete=TRUE) +
+      scale_fill_viridis(name='Watershed', discrete=TRUE)
+  })
 
-  # ## Tab 2
+  ## Tab 2
+  
+      output$plot2 <- renderPlot({
+        yvarnames <- c('pH','oxygen_concentration_in_sea_water')
+                ggplot(data = dat_WQ,
+               aes_string(x = 'End_Date', y = yvarnames[input$WQ_param])) +
+          geom_point() +
+                  labs(x = "Year", y = input$y) +
+          geom_smooth(method = 'loess', se = T) +
+          theme_minimal() +
+          theme(text = element_text(size=18),
+                legend.position = 'bottom')
+      })
+    
+  
+
+    
+# 
+#  plot2data <- reactive({
+#     if (input$WQ_param == 'pH'){
+#       ds1 <-dat_WQ %>% select(pH)
+#     }
+#     else if (input$WQ_param == 'oxygen_concentration_in_sea_water'){
+#       ds1 <-dat_WQ %>% select(oxygen_concentration_in_sea_water)
+#     }
+#     return(ds1)
+#   })
+
+ # plot2label<- reactive({
+ #   if (input$WQ_param == 'pH'){
+ #     lab1 <-'pH'
+ #   }
+ #   else if (input$WQ_param == 'oxygen_concentration_in_sea_water'){
+ #     lab1 <-'dissolved oxygen (mg/L)'
+ #   }
+ #   return(lab1)
+ # })
+ # 
+ #  output$plot2<- renderPlot({
+ #    ggplot(dat_WQ, aes(x=Start_Date, y=input$WQ_param))#+
+ #     ylab(plot2label())
+ # 
+ #  })
+
+
   # # Reactive expression for the data subsetted to what the user selected
   # # column_se<-reactive({
   # #   par_se<-column_par_se<-[input$WQ_param]
@@ -261,12 +297,12 @@ server <- function(input, output, session) {
   #     #   'Monthly N load (kg N ',ha^-1,yr^-1,')')))+
   #     xlab('Date')+
   #     geom_point(alpha = 0.7) +
-  #     geom_smooth(method = "loess", se = T) +
+  #     geom_smooth(method = 'loess', se = T) +
   #     theme_minimal() +
   #     theme(text = element_text(size=18),
-  #           legend.position = "none")+
-  #     scale_color_viridis(name="Station Name", discrete=TRUE) +
-  #     scale_fill_viridis(name="Station Name", discrete=TRUE)
+  #           legend.position = 'none')+
+  #     scale_color_viridis(name='Station Name', discrete=TRUE) +
+  #     scale_fill_viridis(name='Station Name', discrete=TRUE)
   # )
 
   # render plot3
@@ -279,12 +315,12 @@ server <- function(input, output, session) {
   #       'Monthly N load (kg N ',ha^-1,yr^-1,')')))+
   #     xlab('Date')+
   #     geom_point(alpha = 0.7) +
-  #     geom_smooth(method = "loess", se = T) +
+  #     geom_smooth(method = 'loess', se = T) +
   #     theme_minimal() +
   #     theme(text = element_text(size=18),
-  #           legend.position = "bottom")+
-  #     scale_color_viridis(name="Watershed", discrete=TRUE) +
-  #     scale_fill_viridis(name="Watershed", discrete=TRUE)
+  #           legend.position = 'bottom')+
+  #     scale_color_viridis(name='Watershed', discrete=TRUE) +
+  #     scale_fill_viridis(name='Watershed', discrete=TRUE)
   # )
 }
 
@@ -293,3 +329,4 @@ server <- function(input, output, session) {
 
 #options(shiny.reactlog = TRUE)
 shinyApp(ui, server)
+
