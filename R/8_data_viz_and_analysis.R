281de8d760e32c2444a3eee37303eeba7eed8b01
Alex Huddell <48715797+ahuddell@users.noreply.github.com>
2022-06-13 10:58:45 -0400

    corrected bootsrapping approach to sample from first 10 years of data and rejoin to dataframe

--- R/8_data_viz_and_analysis.R
+++ R/8_data_viz_and_analysis.R
@@ -8,7 +8,7 @@ library(tsibble)
 library(dataRetrieval)
 library(broom)
 library(imputeTS)
-library(fable)
+#library(fable)
 
 #load data
 dat<-read_csv(file=here("data","clean_PCS_ECHO_dat.csv"))
@@ -19,111 +19,6 @@ dat<-dat %>%
   mutate(month=str_remove(month, "^0+"))%>%
   mutate(key2=paste0(permit_outfall,'_',month,'_',year))
 
-# load CHIRPS precipitation data ------------------------------------------
-precip<-read_csv(here('data','WWTP_CHIRPS.csv'))
-
-precip<-precip %>%
-  select(-`system:index`)%>%
-  pivot_longer(
-    cols=('0_0_01_01_1989':'9_9_10_01_1998'),
-    names_to='ID',
-    values_to = 'monthly_precip_mm') %>%
-  separate(ID, c(NA, NA, 'month', 'day','year')) %>%
-  mutate(month=str_remove(month, "^0+"))%>%
-  mutate(key2=paste0(permit_outfall,'_',month,'_',year)) %>%
-  select(permit_outfall,month,year,monthly_precip_mm,key2)
-
-
-
-join<-left_join(dat,precip, by='key2')
-join
-
-#relationship between monthly precip versus raw monthly precipitation
-ggplot(join, aes(x=monthly_precip_mm, y=kg_N_TN_per_month),
-       col=as.factor(permit_outfall))+
-  geom_point() +
-  theme_minimal()
-
-summary(lm(kg_N_TN_per_month~monthly_precip_mm, data=join))
-
-#relationship between % change from the mean (for each facility) versus 
-#monthly precipitation
-
-mean_N_load<-join %>%
-  group_by(permit_outfall.x) %>%
-  summarise(mean_N_load=mean(kg_N_TN_per_month, na.rm=T)) 
-
-join2<-left_join(join,mean_N_load)
-
-join2<- join2 %>%
-        mutate(pct_change_N_load=(kg_N_TN_per_month-mean_N_load)/
-                                  mean_N_load*100
-               )
-  
-
-
-#relationship between % change in monthly N loads versus raw monthly precipitation
-join2 %>%
-    filter(state=='CT') %>%
-    ggplot( aes(x=monthly_precip_mm, y=pct_change_N_load))+
-  geom_point() +
-  geom_smooth(method='loess')+
-  theme_minimal() +
-  ylab('% change in N load')+
-  xlab('Monthly precipitation (mm)')+
-  facet_wrap(~facility, scales = 'free')
-
-join2 %>%
-  filter(state!='CT') %>%
-  ggplot( aes(x=monthly_precip_mm, y=pct_change_N_load))+
-  geom_point() +
-  geom_smooth(method='loess')+
-  theme_minimal() +
-  ylab('% change in N load')+
-  xlab('Monthly precipitation (mm)')+
-  facet_wrap(~facility, scales = 'free')
-
-#same plot without facet for all facilities
-join2 %>%
-  ggplot( aes(x=monthly_precip_mm, y=pct_change_N_load))+
-  geom_point() +
-  geom_smooth(method='loess')+
-  theme_minimal() +
-  ylab('% change in N load')+
-  xlab('Monthly precipitation (mm)')
-
-
-# ###these models are not converging
-# #random intercepts only
-# pct_N_load_model1<-lmer(pct_change_N_load~monthly_precip_mm + 
-#                         (1| facility), data = join2)
-# 
-# summary(pct_N_load_model1)
-# ranef(pct_N_load_model1)
-# 
-# #random slopes and intercepts for each model
-# pct_N_load_model2<-lmer(pct_change_N_load~(monthly_precip_mm | permit_outfall.x), 
-#                        data = join2)
-# summary(pct_N_load_model2)
-# ranef(pct_N_load_model2)
-
-#perhaps it makes more sense to fit separate lms for each outfall
-
-
-by_outfall <- join2 %>%
-              drop_na(pct_change_N_load, monthly_precip_mm) %>%
-              group_by(permit_outfall.x)
-
-fitted_models<-do(by_outfall, 
-       glance( 
-            lm(pct_change_N_load ~ monthly_precip_mm, data = .)))
-
-hist(fitted_models$adj.r.squared)
-
-hist(fitted_models$r.squared)
-
-hist(fitted_models$p.value)
-
 
 # impute time series missing data -----------------------------------------
 
@@ -133,19 +28,19 @@ dat$kg_N_TN_per_month<-round(dat$kg_N_TN_per_month,2)
 
 #drop day from month year format
 dat$month_year<-yearmonth(dat$month_year)
-
-#calculate rolling mean in separate column
-dat<-dat %>%
-  group_by(permit_outfall) %>% 
-  arrange(month_year) %>%
-  mutate(rollingmean=rollmean(kg_N_TN_per_month, k=6, fill=NA)) #calculate rolling mean
-
-ggplot(dat, aes(x=kg_N_TN_per_month, y=rollingmean))+
-  geom_point()+
-  geom_abline(slope=1)+
-  annotate('text', x=10^5,y=10^5.8,label='R2= 0.99', fontface=2)
-
-summary(lm(dat$rollingmean~dat$kg_N_TN_per_month))
+# 
+# #calculate rolling mean in separate column
+# dat<-dat %>%
+#   group_by(permit_outfall) %>% 
+#   arrange(month_year) %>%
+#   mutate(rollingmean=rollmean(kg_N_TN_per_month, k=6, fill=NA)) #calculate rolling mean
+# 
+# ggplot(dat, aes(x=kg_N_TN_per_month, y=rollingmean))+
+#   geom_point()+
+#   geom_abline(slope=1)+
+#   annotate('text', x=10^5,y=10^5.8,label='R2= 0.99', fontface=2)
+# 
+# summary(lm(dat$rollingmean~dat$kg_N_TN_per_month))
 
 #create tsibble object
 dat_ts<-as_tsibble(dat, key = permit_outfall, index=month_year)
@@ -153,7 +48,6 @@ dat_ts
 
 #inspecting missingingness
 has_gaps(dat_ts)
-scan_gaps(dat_ts)
 ts_gaps<-count_gaps(dat_ts)
 
 #visualizing time gaps
@@ -167,16 +61,25 @@ ggplot(ts_gaps, aes(x = permit_outfall)) +
   theme(legend.position = "bottom")
 
 
-# filling in time gaps with imputeTS -----------------------
+# filling in time gaps with imputeTS ----------------------
 
-
-#impute missing values with previous estimate in time series with imputeTS
+#impute missing values within the bounds of the original time series for each outfall
+#with imputeTS "na_kalman" function 
 full_ts<- dat_ts %>%
   select(permit_outfall,month_year,kg_N_TN_per_month) %>% #removing unnecessary columns
+  fill_gaps(.full = FALSE) %>% #fill in NAs in missing months within the bounds of each outfall's time series
   group_by_key() %>% 
   mutate(imputed_missing_value=if_else(is.na(kg_N_TN_per_month), 1, 0)) %>% #designate imputed values as "1"
   mutate(kg_N_TN_per_month_complete=kg_N_TN_per_month) %>% #first, copy over original data
-  mutate(kg_N_TN_per_month_complete_1=na_kalman(kg_N_TN_per_month)) #impute missing data in "kalman imputed"
+  mutate(kg_N_TN_per_month_complete=na_kalman(kg_N_TN_per_month)) #impute missing data in "kalman imputed"
+
+
+#inspecting missingingness
+summary(has_gaps(full_ts)$.gaps)
+#now there are no time series gaps in full_ts object
+
+# backfill each time series further back in time --------------------------------------------------------------------
+
 
 #next we want to determine the length of data that need to be filled in for each
 #permit_outfall time series for the beginning of the series
@@ -185,65 +88,116 @@ full_ts<- dat_ts %>%
 # was available;
 set.seed(4561)
 
-#create small df with permit/outfall and date of first observation
-min_year<-as_tibble(full_ts) %>%
-          group_by(permit_outfall) %>%
-          select(permit_outfall,month_year) %>%
-          summarise(min_time_step=min(month_year)) %>%
-          mutate(time_gap_interval=min_time_step-min(min_time_step)) %>% #calculate length of time interval to fill in later
-          select(-min_time_step)
-min_year
+# #create small df with permit/outfall and date of first observation
+# min_year<-as_tibble(full_ts) %>%
+#           drop_na(kg_N_TN_per_month) %>% #drop NAs in kg_N_TN_per_month
+#           group_by(permit_outfall) %>%
+#           select(permit_outfall,month_year) %>%
+#           summarise(min_time_step=min(month_year)) %>%
+#           mutate(time_gap_interval=min_time_step-min(min_time_step)) #calculate length of time interval to fill in later
+# min_year
+
+#create empty rows for missing data from the beginning of the time series to fill later
+start_ts_NA <-full_ts %>%
+  fill_gaps(.full = start()) %>% #fill in NAs in missing months to the same starting point
+  mutate(imputed_missing_value=if_else(is.na(kg_N_TN_per_month), 1, 0)) %>% #designate imputed values as "1"
+  filter(is.na(kg_N_TN_per_month_complete)) %>%
+  select(-kg_N_TN_per_month_complete)
+
+#summarizes number of time gaps to fill per permit/outfall
+start_ts_NA_gaps<-as_tibble(start_ts_NA) %>%
+  group_by(permit_outfall) %>%
+  summarise(nrow_fill_gaps=n())
+
+start_ts_NA_gaps
+
+#################################################
           
-bootstrap_first_10yr<-as_tibble(full_ts) %>%
+#sample from first 10 years of each distribution (n=nrow_fill_gaps from previous step)
+data_first_10yr<-
+  as_tibble(full_ts) %>%
   ungroup() %>%
   drop_na(kg_N_TN_per_month) %>% #drop NAs in kg_N_TN_per_month
-  mutate(year=year(month_year),
-         month=month(month_year)) %>%
+  mutate(year=year(month_year)) %>%
+  group_by(permit_outfall) %>% 
   filter(year>=min(year) & year<=min(year)+10) %>% #filter each group to first 10 years
-  select(permit_outfall,kg_N_TN_per_month) %>% #select permit_outfall_month column and N load
-  nest(-permit_outfall) %>% 
-  mutate(permit_outfall_2=permit_outfall) %>%
-  separate(permit_outfall_2,into=c('permit','outfall',NA),sep='_') %>% #separate permit and outfall
-  mutate(permit_outfall=paste0(permit,'_',outfall)) %>% #rejoin permit and outfall
-  left_join(min_year) %>% #join minimum time step to each permit_outfall
-  #mutate(n=rep(20)) %>% 
-  mutate(bootstrap_first_10yr = map2(data, time_gap_interval, sample_n, replace = TRUE)) %>% 
-  select(-data, -permit, -outfall, -time_gap_interval) %>% #remove original data
-  unnest
-
-bootstrap_first_10yr
+  select(permit_outfall,kg_N_TN_per_month)  %>%
+  left_join(start_ts_NA_gaps) 
 
 
-#the total of the time gaps across each permit/outfall should be the same 
-#as the length of the bootstrap df
-sum(min_year$time_gap_interval)
+nested_dat<-data_first_10yr %>%
+  nest(data = (kg_N_TN_per_month))
+  
+
+
+bootstrap_first_10yr <- nested_dat %>%
+  drop_na(nrow_fill_gaps) %>% #drop the one permit/outfall that isn't missing anything
+  #sample the data column (observations from first ten years of data) using the
+  #nrow_fill_gaps column as the size to create the right size tibble to rejoin
+  mutate(samp = map2(data, nrow_fill_gaps, sample_n, replace=T)) %>% 
+  select(-data, -nrow_fill_gaps) %>% #remove unwanted columns
+  unnest(cols=c(samp)) %>%
+  group_by(permit_outfall) %>%
+  mutate(n = row_number()) %>% #number rows by group
+  mutate(permit_outfall_n=paste0(permit_outfall,'_',n)) %>% #add unique row ID %>%
+  mutate(boostrapped_dat=kg_N_TN_per_month) %>%
+  select(-n) 
+
+#check the dimensions are the same between the bootstrapped values and the 
+#time gap interval that we are supposed to fill
+sum(start_ts_NA_gaps$nrow_fill_gaps)
 dim(bootstrap_first_10yr)
+dim(start_ts_NA)
+
+summary(bootstrap_first_10yr$boostrapped_dat)
+
+# insert bootstrapped data into empty start_ts_NA dataframe  --------------
+
+#check the length of both dataframes and that all data are missing from start_ts_NA
+nrow(bootstrap_first_10yr)
+nrow(start_ts_NA)
+summary(start_ts_NA$kg_N_TN_per_month)
+
+unique(bootstrap_first_10yr$permit_outfall)
+unique(start_ts_NA$permit_outfall)
 
 #join bootstrapped data back to time series data data frame
+start_ts_NA2<-
+  as_tibble(start_ts_NA) %>%
+  group_by(permit_outfall) %>%
+  mutate(n = row_number()) %>%
+  mutate(permit_outfall_n=paste0(permit_outfall,'_',n)) %>% #add unique row ID
+  left_join(bootstrap_first_10yr, by="permit_outfall_n") %>% #join the bootstrap tibble
+  mutate(kg_N_TN_per_month_complete=boostrapped_dat)  %>% #overwrite the NA values with the bootstrapped values 
+  mutate(permit_outfall= permit_outfall.x) %>%
+  select(permit_outfall, month_year, kg_N_TN_per_month,
+          imputed_missing_value, kg_N_TN_per_month_complete)
 
-fill_gaps(.full = TRUE) #fill in NAs in missing months from outside of the time range
+start_ts_NA2
+summary(start_ts_NA2$kg_N_TN_per_month_complete)
 
-  
-bootstap_95_00<-full_ts %>%
-    mutate(year=year(month_year),
-           month=month(month_year))%>%
-    filter(year>=1995 & year<=2000)%>% #filter to between 1989-1994
-    group_by(permit_outfall, month) %>% #group by month of the year and month
-    sample_n(!is.na(kg_N_TN_per_month), size=5, replace=TRUE)
 
-summary(full_ts$kg_N_TN_per_month_complete) #there are no NAs
+# rejoin the boostrapped, early observations to the later observations --------
+
+full_ts<-as_tibble(full_ts) 
+nrow(full_ts) #31k
+summary(full_ts$kg_N_TN_per_month_complete)
+
+
+full_ts<-bind_rows(full_ts,start_ts_NA)
+nrow(full_ts) #53k
+
+
 
 #quantify how many data were imputed
 ((table(full_ts$imputed_missing_value)[[2]])/
-    (table(full_ts$imputed_missing_value)[[1]]+table(full_ts$imputed_missing_value)[[2]]))*100
-#53% of the data
+    (table(full_ts$imputed_missing_value)[[1]]+
+       table(full_ts$imputed_missing_value)[[2]]))*100
+#48% of the data
 
-  
 
-  
 
 # rejoin other data to completed time series ------------------------------
-
 
 #organizing permit, facility, Outfall, and state by each permit_outfall to rejoin 
 data_to_rejoin<- dat %>%
@@ -275,6 +229,41 @@ full_ts<-left_join(full_ts,data_to_rejoin2)
 names(full_ts)
 
 
+# exclude data before permit origination date ----------------------------
+
+
+#load permit origination data
+permit_dates<-read_csv(file=here("data","permit_origination_dates.csv"))
+permit_dates<-permit_dates %>%
+              mutate(orig_issue_dt=ymd(orig_issue_dt)) %>%           
+              select(permit, orig_issue_dt) #select columns needed
+permit_dates #some fail to parse because not all permits had origination dates
+
+#join permit dates 
+full_ts<-left_join(full_ts,permit_dates)
+
+#check whether any observations from before the permit origin issue date
+check_permit_dates<-full_ts %>%
+  group_by(permit) %>%
+  mutate(obs_date=as_date(month_year))%>%
+  filter(obs_date<=orig_issue_dt) %>% #remove early data from before permit origination
+  mutate(remove_ID=paste0(permit_outfall,month_year)) #create a tag to remove these rows
+nrow(check_permit_dates)
+#488 observations do come before permit observation date and should be excluded
+
+observations_to_remove=check_permit_dates$remove_ID
+
+#remove observations that precede permit origination date
+nrow(full_ts) #53,097
+
+full_ts<-full_ts %>%
+  mutate(check_ID=paste0(permit_outfall,month_year)) %>%
+  filter(!check_ID %in% observations_to_remove) %>% #remove observations from before or at permit origin issue date
+  select(-check_ID) #then remove those columns
+
+nrow(full_ts) #52609    
+
+#488 observations removed
 
 # join TMDL zones to data and add water year-------------------------------------------------
 
@@ -282,7 +271,7 @@ TMDL_zones<-read_csv(file=here("data",'TMDL_zones.csv'))
 
 full_ts<-left_join(full_ts,TMDL_zones)
 
-
+#calculate water year
 full_ts$water_year<-calcWaterYear(full_ts$month_year)
 
 # plot time series --------------------------------------------------------
